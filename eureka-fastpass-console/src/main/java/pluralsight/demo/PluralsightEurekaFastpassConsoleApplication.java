7169ac3c9c4364daf44c7bf33cf7a47c658f5912
TheGaneshkumawat <kumawat.ganesh12@gmail.com>
2020-04-30 19:18:40 +0530

    Initial commit

---
+++ eureka-fastpass-console/src/main/java/pluralsight/demo/PluralsightEurekaFastpassConsoleApplication.java
@@ -0,0 +1,16 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+//	@EnableCircuitBreaker
+public class PluralsightEurekaFastpassConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightEurekaFastpassConsoleApplication.class, args);
+	}
+}
